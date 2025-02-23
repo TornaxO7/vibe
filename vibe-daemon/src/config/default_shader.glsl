7c9d30e0aaf11979d69b648e42cc42c0bbb9c370
TornaxO7 <tornax@proton.me>
2025-02-23 12:08:15 +0100

    make default shader more readable

--- vibe-daemon/src/config/default_shader.glsl
+++ vibe-daemon/src/config/default_shader.glsl
@@ -13,27 +13,29 @@ layout(binding = 2) uniform float iTime;
 // the color which the pixel should have
 layout(location = 0) out vec4 fragColor;
 
-void main() {
-    float gamma = 2.2;
-    float m = 0.;
+const float GAMMA = 2.2;
+const vec3 WHITE = vec3(1.);
 
+void main() {
     vec2 uv = gl_FragCoord.xy / iResolution.xy;
+    // flip y value so that 0 is the bottom and 1 the top
+    float y = 1. - uv.y;
 
     vec2 zuv = uv * float(freqs.length());
     int id = int(floor(zuv.x));
-    float y = 1. - uv.y;
 
-    m += step(y, freqs[id]);
+    // only draw something if we are within a bar
+    if (y <= freqs[id]) {
+        float pixel_strength = step(y, freqs[id]);
+        vec3 bottom_color = sin(vec3(12., 23., 34.) + iTime) * .2 + .5;
+        vec3 top_color = WHITE;
 
-    vec3 bottom_color = sin(vec3(12., 23., 34.) + iTime) * .2 + .5;
-    vec3 top_color = vec3(1.);
+        vec3 col = mix(bottom_color, top_color, y) * pixel_strength;
 
-    if (m > 0.) {
-        vec3 col = mix(bottom_color, top_color, y) * m;
         // apply gamma correction
-        col.x = pow(col.x, gamma);
-        col.y = pow(col.y, gamma);
-        col.z = pow(col.z, gamma);
+        col.x = pow(col.x, GAMMA);
+        col.y = pow(col.y, GAMMA);
+        col.z = pow(col.z, GAMMA);
         fragColor = vec4(col, y);
     }
 }
