8833dab6deff9149d3e0b2ed131790820414894b
TornaxO7 <tornax@proton.me>
2025-03-06 11:17:33 +0100

    change default shader to make it more bar looking

--- src/output/shaders/default.glsl
+++ src/output/shaders/default.glsl
@@ -4,13 +4,19 @@
     vec2 uv = gl_FragCoord.xy / iResolution.xy;
 
     vec2 zuv = uv * float(freqs.length());
-    int id = int(floor(zuv.x));
+    float x = fract(zuv.x);
     float y = 1. - uv.y;
 
+    int id = int(floor(zuv.x));
+    float freq = freqs[id] * .75;
+
+    // left and right space of a bar
+    float space = 0.2;
+    
     // check if we are within a bar
-    if (y <= freqs[id]) {
-        vec3 bottom_color = sin(vec3(2., 4., 8.) + iTime) * .2 + .5;
-        float presence = step(y, freqs[id]);
+    if (y <= freq && space < x && x < 1. - space) {
+        vec3 bottom_color = sin(vec3(2., 4., 8.) * iTime * .25) * .2 + .6;
+        float presence = step(y, freq);
     
         vec3 col = mix(bottom_color, WHITE, y) * presence;
     
@@ -18,6 +24,5 @@
         col.x = pow(col.x, GAMMA);
         col.y = pow(col.y, GAMMA);
         col.z = pow(col.z, GAMMA);
-    
         fragColor = vec4(col, y);
     }
