4f5c21294fe0f246cfe2eb60f910f7083bebb651
TornaxO7 <tornax@proton.me>
2025-08-27 15:05:34 +0200

    test(chessy): add chessy test

---
+++ vibe-renderer/tests/chessy/mod.rs
@@ -0,0 +1,25 @@
+use vibe_audio::{fetcher::DummyFetcher, BarProcessorConfig, SampleProcessor};
+use vibe_renderer::components::{Chessy, ChessyDescriptor, SdfPattern};
+
+use crate::Tester;
+
+#[test]
+fn test() {
+    let mut tester = Tester::default();
+
+    let sample_processor = SampleProcessor::new(DummyFetcher::new(2));
+    let chessy = Chessy::new(&ChessyDescriptor {
+        renderer: &tester.renderer,
+        sample_processor: &sample_processor,
+        audio_config: BarProcessorConfig::default(),
+        texture_format: tester.output_texture_format(),
+        movement_speed: 0.01,
+        pattern: SdfPattern::Heart,
+        zoom_factor: 0.1,
+    });
+
+    let _img = tester.render(chessy);
+
+    // we don't do anything else because all bars are at the bottom
+    // but the fragment shader should work... trust me bro
+}
--- vibe-renderer/tests/mod.rs
+++ vibe-renderer/tests/mod.rs
@@ -18,6 +18,8 @@ mod circle_graph;
 mod radial_color_variant;
 mod radial_height_gradient_variant;
 
+mod chessy;
+
 const PIXEL_SIZE: u32 = std::mem::size_of::<u32>() as u32;
 
 // some colors
