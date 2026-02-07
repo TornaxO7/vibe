f5c803c4596f4cb3dae5b4de5b43601bf0f55c68
TornaxO7 <tornax@proton.me>
2026-02-07 01:14:17 +0100

    refactor(vibe): split up vibe into a lib and binary part

--- vibe/src/main.rs
+++ vibe/src/main.rs
@@ -1,25 +1,11 @@
 mod cli;
-mod config;
-mod output;
-mod state;
-mod types;
-mod window;
-
-use std::{path::PathBuf, sync::OnceLock};
 
 use clap::Parser;
-use state::State;
 use tracing::{error, info};
 use tracing_indicatif::IndicatifLayer;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
+use vibe::{window, State};
 use wayland_client::{globals::registry_queue_init, Connection};
-use xdg::BaseDirectories;
-
-const APP_NAME: &str = env!("CARGO_PKG_NAME");
-const OUTPUT_CONFIG_DIR_NAME: &str = "output_configs";
-const CONFIG_FILE_NAME: &str = "config.toml";
-
-static XDG: OnceLock<BaseDirectories> = OnceLock::new();
 
 fn main() -> anyhow::Result<()> {
     init_logging();
@@ -82,21 +68,4 @@ fn init_logging() {
         .init();
 
     tracing::debug!("Debug logging enabled");
-}
-
-fn get_xdg() -> &'static BaseDirectories {
-    XDG.get_or_init(|| BaseDirectories::with_prefix(APP_NAME))
-}
-
-/// Returns the path to the directory where the config files of each output lies.
-/// Each config file has the form `<output-name>.toml`.
-pub fn get_output_config_dir() -> PathBuf {
-    get_xdg()
-        .create_config_directory(OUTPUT_CONFIG_DIR_NAME)
-        .unwrap()
-}
-
-/// Returns the path to the config file of `vibe`.
-pub fn get_config_path() -> PathBuf {
-    get_xdg().place_config_file(CONFIG_FILE_NAME).unwrap()
 }
