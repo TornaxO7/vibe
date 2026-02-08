c46de1a7067d18f134442d8beadcbd7f6005152f
TornaxO7 <tornax@proton.me>
2026-02-08 11:37:15 +0100

    test(bar_presence_gradient): add bar_presence_gradient to new test framework

--- vibe-renderer/tests/bar_presence_gradient_variant/mod.rs
+++ vibe-renderer/tests/bar_presence_gradient_variant/mod.rs
@@ -1,4 +1,4 @@
-use vibe_audio::{fetcher::DummyFetcher, BarProcessorConfig, SampleProcessor};
+use vibe_audio::BarProcessorConfig;
 use vibe_renderer::components::{BarVariant, Bars, BarsDescriptor, BarsFormat};
 
 use crate::{Tester, BLUE, RED};
@@ -7,10 +7,9 @@ use crate::{Tester, BLUE, RED};
 fn test() {
     let tester = Tester::default();
 
-    let sample_processor = SampleProcessor::new(DummyFetcher::new(2));
-    let bars = Bars::new(&BarsDescriptor {
+    let mut bars = Bars::new(&BarsDescriptor {
         renderer: &tester.renderer,
-        sample_processor: &sample_processor,
+        sample_processor: &tester.sample_processor,
         audio_conf: BarProcessorConfig::default(),
         texture_format: tester.output_texture_format(),
         max_height: 1.,
@@ -23,8 +22,10 @@ fn test() {
     })
     .unwrap_or_else(|msg| panic!("{}", msg));
 
-    let _img = tester.render(&bars);
-
-    // we don't do anything else because all bars are at the bottom
-    // but the fragment shader should work... trust me bro
+    tester.evaluate(
+        &mut bars,
+        include_bytes!("./reference.png"),
+        "bar-presence-gradient",
+        0.9,
+    );
 }
