9b480a1f45ad11318a46276f68848cd1d1ad4c71
TornaxO7 <tornax@proton.me>
2026-02-08 12:43:20 +0100

    test(pulse-edges): move to new test framework

--- vibe-renderer/tests/live_wallpaper_pulse_edges/mod.rs
+++ vibe-renderer/tests/live_wallpaper_pulse_edges/mod.rs
@@ -1,16 +1,11 @@
-use std::num::NonZero;
-
-use image::ImageReader;
-use vibe_audio::{fetcher::DummyFetcher, SampleProcessor};
-use vibe_renderer::components::live_wallpaper::pulse_edges::{PulseEdges, PulseEdgesDescriptor};
-
 use crate::Tester;
+use image::ImageReader;
+use std::num::NonZero;
+use vibe_renderer::components::live_wallpaper::pulse_edges::{PulseEdges, PulseEdgesDescriptor};
 
 #[test]
 fn test() {
     let tester = Tester::default();
-
-    let sample_processor = SampleProcessor::new(DummyFetcher::new(2));
 
     let img = ImageReader::open("../assets/castle.jpg")
         .unwrap()
@@ -19,24 +14,26 @@ fn test() {
 
     let mut pulse_edges = PulseEdges::new(&PulseEdgesDescriptor {
         renderer: &tester.renderer,
-        sample_processor: &sample_processor,
+        sample_processor: &tester.sample_processor,
         texture_format: tester.output_texture_format(),
 
         img,
-        freq_range: NonZero::new(100).unwrap()..NonZero::new(250).unwrap(),
+        freq_range: NonZero::new(100).unwrap()..NonZero::new(300).unwrap(),
         audio_sensitivity: 4.0,
-        high_threshold_ratio: 0.6,
+        high_threshold_ratio: 0.7,
         low_threshold_ratio: 0.3,
-        wallpaper_brightness: 0.5,
+        wallpaper_brightness: 0.2,
         edge_width: 4.0,
-        pulse_brightness: 5.0,
-        sigma: 10.,
-        kernel_size: 49,
+        pulse_brightness: 4.0,
+        sigma: 1.,
+        kernel_size: 9,
     })
     .unwrap();
 
-    let _img = tester.render(&mut pulse_edges);
-
-    // we don't do anything else because all bars are at the bottom
-    // but the fragment shader should work... trust me bro
+    tester.evaluate(
+        &mut pulse_edges,
+        include_bytes!("./reference.png"),
+        "pulse-edges",
+        0.68,
+    );
 }
