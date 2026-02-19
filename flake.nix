f86ad05d983e0c9d3187ca4397c4ef0ee70dfd33
TornaxO7 <tornax@proton.me>
2026-02-19 10:48:05 +0100

    build(vulkan-loader): override vulkan-loader with fork

--- flake.nix
+++ flake.nix
@@ -3,8 +3,6 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-parts.url = "github:hercules-ci/flake-parts";
-
-    pwndbg.url = "github:pwndbg/pwndbg";
   };
 
   outputs = inputs@{ flake-parts, ... }:
@@ -21,6 +19,44 @@
 
             overlays = with inputs; [
               rust-overlay.overlays.default
+
+              (final: prev:
+                let
+                  version = "1.4.343.0";
+                in
+                {
+                  vulkan-loader = prev.vulkan-loader.overrideAttrs (old: {
+                    inherit version;
+                    src = prev.fetchFromGitHub {
+                      owner = "charles-lunarg";
+                      repo = "Vulkan-Loader";
+                      rev = "5b0ed940054d35d2b07840b02c1b8e0c2bf2e5cc";
+                      hash = "sha256-AVd0Q2axESlFVShjNFUFoc1fQfvHP3/QfNtbD92jujg=";
+                    };
+                  });
+
+                  vulkan-headers = prev.vulkan-headers.overrideAttrs (old: {
+                    inherit version;
+                    src = prev.fetchFromGitHub {
+                      owner = "KhronosGroup";
+                      repo = "Vulkan-Headers";
+                      rev = "49f1a381e2aec33ef32adf4a377b5a39ec016ec4";
+                      hash = "sha256-K/8X9D7B0o6+osOzScplwea+OsfM3srAtDKCgfZpcJU=";
+                    };
+                  });
+
+                  # vulkan-validation-layers = prev.vulkan-validation-layers.overrideAttrs (old: {
+                  #   inherit version;
+                  #   src = pkgs.fetchFromGitHub {
+                  #     owner = "KhronosGroup";
+                  #     repo = "Vulkan-ValidationLayers";
+                  #     rev = "11440fc0664718ac51646c63d6e321f61195e808";
+                  #     hash = "sha256-iAOUwTAU8VdrMNDYlPHWqPKtzDZOHRxNq4nsEmDbsug=";
+                  #   };
+
+                  #   cmakeFlags = old.cmakeFlags ++ [ "-DUPDATE_DEPS=false" ];
+                  # });
+                })
             ];
           };
 
@@ -43,7 +79,6 @@
                     cargo-flamegraph
                     cargo-release
                     git-cliff
-                    inputs.pwndbg.packages.${system}.pwndbg
                   ] ++ [ rust-toolchain ];
 
                   buildInputs = vibe.buildInputs;
